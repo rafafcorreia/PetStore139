3fcfd23399e8eb4cf8bcde9ac51baaf58ec100cb
rafafcorreia <rafa.f.correia@gmail.com>
2023-10-23 20:14:46 -0300

    Adicionado DDT

--- tests/PetTest.cs
+++ tests/PetTest.cs
@@ -1,3 +1,4 @@
+using System.Data.Common;
 using models;
 using Newtonsoft.Json;
 using RestSharp;
@@ -7,6 +8,41 @@ namespace tests;
 public class PetTest
 {
     private const String BASE_URL = "https://petstore.swagger.io/v2/";
+
+    public static IEnumerable<TestCaseData> getTestData()
+    {
+        String caminhoMassa = @"C:\testspace\PetStore139\fixtures\pet.csv";
+
+        /* switch (operacao)
+        {
+            case 1:
+                caminhoMassa = @"C:\testspace\nunit-projeto\Calculadora.Tests\fixtures\massaSomar.csv";
+                break;
+            case 2:
+                caminhoMassa = @"C:\testspace\nunit-projeto\Calculadora.Tests\fixtures\massaSubtrair.csv";
+                break;
+            case 3:
+                caminhoMassa = @"C:\testspace\nunit-projeto\Calculadora.Tests\fixtures\massaMultiplicar.csv";
+                break;
+            case 4:
+                caminhoMassa = @"C:\testspace\nunit-projeto\Calculadora.Tests\fixtures\massaDividir.csv";
+                break;
+        } */
+
+        using (var reader = new StreamReader(caminhoMassa))
+        {
+            // Pula a primeira linha com os cabeçahos
+            reader.ReadLine();
+
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(", ");
+
+                yield return new TestCaseData(int.Parse(values[0]), int.Parse(values[1]), values[2], values[3], values[4], values[5], values[6], values[7]);
+            }
+        }
+    }
 
     private RestClient client;
     [SetUp]
@@ -96,6 +132,48 @@ public class PetTest
         Assert.That((int)responseBody.code, Is.EqualTo(200));
         Assert.That(responseBody.message.ToString(), Is.EqualTo(petId.ToString()));
 
-        Console.WriteLine("Variável de ambiente: " + Environment.GetEnvironmentVariable("petId"));
+        Console.WriteLine($"Variável de ambiente (petId): {Environment.GetEnvironmentVariable("petId")}");
+    }
+
+    [TestCaseSource("getTestData", new object[] { }), Order(5)]
+    public void PostPetTestDDT(int petId, int categoryId, String categoryName, String petName, String photoUrls, String tagsIds, String tagsNames, String status)
+    {
+        // var client = new RestClient(BASE_URL);
+        var request = new RestRequest("pet", Method.Post);
+
+        PetModel petModel = new PetModel();
+        petModel.id = petId;
+        petModel.category = new Category(categoryId, categoryName);
+        petModel.name = petName;
+        petModel.photoUrls = new String[] { photoUrls };
+
+        String[] tagsIdsList = tagsIds.Split("; ");
+        String[] tagsNamesList = tagsNames.Split("; ");
+        List<Tag> tagsList = new List<Tag>();
+
+        petModel.tags = new Tag[tagsIds.Length];
+
+        for (int i = 0; i < tagsIdsList.Length; i++)
+        {
+            int tagId = int.Parse(tagsIdsList[i]);
+            String tagName = tagsNamesList[i];
+            
+            Tag tag = new Tag(tagId, tagName);
+            tagsList.Add(tag);
+        }
+        petModel.tags = tagsList.ToArray();
+        petModel.status = status;
+        String jsonString = JsonConvert.SerializeObject(petModel, Formatting.Indented);
+        Console.WriteLine(jsonString);
+        request.AddBody(jsonString);
+
+        var response = client.Execute(request);
+        var responseBody = JsonConvert.DeserializeObject<dynamic>(response.Content);
+
+        Assert.That((int)response.StatusCode, Is.EqualTo(200));
+        String respStatus = responseBody.status;
+        Assert.That(respStatus, Is.EqualTo(status));
+        // int petId = responseBody.id;
+        // Environment.SetEnvironmentVariable("petId", petId.ToString());
     }
 }
